2339e7d505d3510a1a419d3e6cf52282d0ede537
Dithilli <41286037+Dithilli@users.noreply.github.com>
2018-07-25 15:17:37 -0700

    Update hiwe2.py
    Updated datetime to just print the formatted time. Could simply not figure out how to pull the time from the request object in a WSGI environment.

--- hiwe2.py
+++ hiwe2.py
@@ -1,21 +1,21 @@
 """
-hiwe2.py is a "hello world" micro webservice that creates a Bottle micro-framework for web applications, and can take a single get to its
-solitary endpoint. It then responsds with "Hello WeWork ang greetings from <Name>", where name is either a given default, or set 
-by the user using an environment variable, or a command line argument. HiWe will prioritize command line arguments over environment variables.
-
+hiwe2.py is a "hello world" micro webservice that creates a Bottle micro-framework for web applications,
+and then responds with "Hello WeWork ang greetings from Name", where name is either default, or set by the user using
+an environment variable, or a command line argument.
 
 It also outputs to STDOUT a log that contains the date and time, the endpoint reached, and the http status code.
 """
 
 
 import os, sys
-from bottle import route, run, response, request
+from datetime import datetime
+from bottle import Bottle, route, run, response, request
 
 """
 Function namer first creates the variable name, and then checks to see if there's a command line argument setting name.
 If not, it checks for an environment variable that sets the variable 'name'. If not, it sets the default to "David".
 
-Considered using ArgParse module for inputting command line arguments, but this just didn't need it. 
+Considered using ArgParse for inputting command line arguments, but this just didn't need it. 
 """
 def namer():
     name = None
@@ -28,14 +28,19 @@ def namer():
     return name
 
 """
-Logger() 
+Function logger creates three variables (code, time, and endpoint) and populates them with information pulled from
+the datetime module, or from the http response object, or request object
 """
 def logger():
-    return request.headers['date'], "Got a request to ", request.path, " endpoint. Replied with ", response.status_code, " code"
+    code = str(response.status_code)
+    time = datetime.now()
+    printtime = time.strftime("%H:%M:%S")
+    endpoint = str(request.fullpath)
+    return printtime + " - Got a request to " + endpoint + " endpoint. Replied with code " + code
 
 """
-The decorator route() is a bottle defined function that runs whatever function is after it whenever a get request hits the defined url. hi() calls namer()
-on the off chance that we wanted to check to see if the name had changed in the environmental variables between gets. 
+So, Bubble has terrible documentation. Should have used Flask. Thought about it, compared them, thought it would be
+simpler for a small project to use Bubble. Learned my lesson. 
 """
 @route('/')
 def hi():
@@ -44,5 +49,6 @@ def hi():
     return "Hello WeWork and greetings from %s!" % name
 
 
-run(host='0.0.0.0', port=5000)
+
+run(host='localhost', port=8080)
 
